fb6bf989ce2d8bffa75b909574ff2981c35c7d3e
Amulya Kuchi <amulyakuchi@gmail.com>
2025-03-06 15:02:02 +0100

    updated the changes for the search viemodel and settings viewmodel

--- lib/viewmodels/search_viewmodel.dart
+++ lib/viewmodels/search_viewmodel.dart
@@ -2,32 +2,12 @@ import 'package:flutter/foundation.dart';
 import '../models/recipe.dart';
 import '../services/spoonacular_api_service.dart';
 
-/// ViewModel for managing recipe search operations.
-///
-/// Handles the loading state, search results, and error messages when fetching recipes.
 class SearchViewModel extends ChangeNotifier {
-  // API service instance for fetching recipe data
   final SpoonacularApiService _apiService = SpoonacularApiService();
-
-  // Loading indicator for search operations
   bool isLoading = false;
-
-  // Error message in case of a failed search
   String errorMessage = '';
-
-  // List of recipes from the search results
   List<Recipe> recipes = [];
 
-  /// Executes a search query with given filters and parameters.
-  ///
-  /// - Parameters:
-  ///   - [ingredients]: Comma-separated list of ingredients to include.
-  ///   - [dietaryPreference]: Diet filter (e.g., Vegan, Gluten-Free).
-  ///   - [numberOfResults]: Maximum number of results to fetch.
-  ///   - [cuisine]: Cuisine filter (e.g., Indian, Italian).
-  ///   - [dishType]: Type of dish filter (e.g., Dessert, Snack).
-  ///
-  /// - Updates recipes with the search results or errorMessage in case of failure.
   Future<void> searchRecipes({
     required String ingredients,
     required String dietaryPreference,
@@ -40,7 +20,6 @@ class SearchViewModel extends ChangeNotifier {
       errorMessage = '';
       notifyListeners();
 
-      // Fetch recipes from the Spoonacular API
       recipes = await _apiService.fetchRecipesByCustomFilters(
         ingredients: ingredients,
         dietaryPreference: dietaryPreference,
@@ -49,7 +28,7 @@ class SearchViewModel extends ChangeNotifier {
         dishType: dishType,
       );
     } catch (e) {
-      errorMessage = e.toString(); // Capture error message
+      errorMessage = e.toString();
     } finally {
       isLoading = false;
       notifyListeners();
