c46c80a556f1a7bcd860219fde1f480bfea731ef
amulyakuchi <amulyakuchi@gmail.com>
2025-03-08 20:36:01 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/material.dart'; // Provides essential Flutter widgets for building UI components
-import 'app.dart'; // Imports the main application file containing the NomNomApp class
+import 'package:flutter/material.dart';
+import 'app.dart';
 
-/// The main function serves as the entry point for the application.
-/// It initializes the Flutter framework and starts the app by running the `NomNomApp` widget.
 void main() {
-  runApp(const NomNomApp()); // Launch the NomNom application.
+  runApp(const NomNomApp());
 }
